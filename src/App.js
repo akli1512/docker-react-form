477fe13fb91af592f4e129eceaaaef3bb26a6e85
Azzout akli <azzoutakli@gmail.com>
2019-04-01 11:49:35 +0200

    'yanis'

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            wqkjkjgggggggggggggggggg;meqwl;meqw
-        </p>
+			has been modified!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	    </p>
       </div>
     );
   }
